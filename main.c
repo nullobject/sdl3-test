06b688d820eecdf2e82b9c8b6dfc2ca9dbf0ea4d
Joshua Bassett <hello@joshbassett.info>
2025-02-03 21:54:55 +1100

    Render a texture

--- main.c
+++ main.c
@@ -2,17 +2,36 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_Texture *texture = NULL;
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-  /* Create the window */
-  if (!SDL_CreateWindowAndRenderer(
-        "Hello World", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-    SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+  if (!SDL_CreateWindowAndRenderer("Hello World",
+                                   WIDTH,
+                                   HEIGHT,
+                                   SDL_WINDOW_RESIZABLE,
+                                   &window,
+                                   &renderer)) {
+    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
+  texture = SDL_CreateTexture(renderer,
+                              SDL_PIXELFORMAT_XRGB8888,
+                              SDL_TEXTUREACCESS_STREAMING,
+                              WIDTH,
+                              HEIGHT);
+
+  if (!texture) {
+    SDL_Log("Couldn't create streaming texture: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
   return SDL_APP_CONTINUE;
 }
 
@@ -26,23 +45,20 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  const char *message = "Hello World!";
-  int w = 0, h = 0;
-  float x, y;
-  const float scale = 4.0f;
+  uint32_t *pixels;
+  int pitch;
 
-  /* Center the message and scale it up */
-  SDL_GetRenderOutputSize(renderer, &w, &h);
-  SDL_SetRenderScale(renderer, scale, scale);
-  x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) /
-      2;
-  y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+  if (!SDL_LockTexture(texture, NULL, (void **)&pixels, &pitch)) {
+    SDL_Log("Couldn't lock texture: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
 
-  /* Draw the message */
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderDebugText(renderer, x, y, message);
+  for (size_t i = 0; i < WIDTH * HEIGHT; i++) {
+    pixels[i] = i;
+  }
+
+  SDL_UnlockTexture(texture);
+  SDL_RenderTexture(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
 
   return SDL_APP_CONTINUE;
